a97ac72793bf2e23f7ec150fc79e54b42d2e873f
Andy Dill <andy@discordapp.com>
2019-11-11 16:34:13 -0800

    Change config.sh to rely on environment variables

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 # returned by the command below:
 # $ xcrun -sdk iphoneos swiftc --version
 
-LLVM_BRANCH="tags/swift-5.3-RELEASE"
+[ -z "$LLVM_BRANCH" ] && LLVM_BRANCH="tags/swift-5.3-RELEASE"
 
 # 2. Select the best branch, tag or commit hash from https://github.com/rust-lang/rust
 
-RUST_BRANCH="tags/1.46.0"
+[ -z "$RUST_BRANCH" ] && RUST_BRANCH="tags/1.46.0"
 
 # 3. Select a name for the toolchain you want to install as. The toolchain will be installed
 # under $HOME/.rustup/toolchains/rust-$RUST_TOOLCHAIN
 
-RUST_TOOLCHAIN="ios-arm64-1.46.0"
+[ -z "$RUST_TOOLCHAIN" ] && RUST_TOOLCHAIN="ios-arm64-1.46.0"
 
